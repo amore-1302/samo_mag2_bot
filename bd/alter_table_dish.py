0b186a3ecf43a6e8d9aa2fa5b16179593fe96e98
Андрей <amore1302@mail.ru>
2024-04-24 19:12:01 +0300

    add alter_table_dish.py alter_table_rest.py

---
+++ bd/alter_table_dish.py
@@ -0,0 +1,30 @@
+"""
+в dishs  таблица блюда
+добавляем столбец  raiting REAL если он там не существует
+
+
+ALTER TABLE dishs ADD COLUMN  raiting REAL
+
+
+"""
+
+
+import sqlite3
+
+
+# Подключаемся к базе данных (или создаем новую)
+conn = sqlite3.connect('../samo_mag_bot.db')
+cursor = conn.cursor()
+
+# Получаем информацию о таблице
+table_name = "dishs"
+column_name = 'raiting'
+cursor.execute(f"PRAGMA table_info({table_name})")
+columns = cursor.fetchall()
+if column_name not in [col[1] for col in columns]:
+    print("Добавляем столбец " + column_name)
+    # Сохраняем изменения в базе данных и закрываем соединение
+    cursor.execute('ALTER TABLE dishs ADD COLUMN  raiting REAL')
+    conn.commit()
+
+conn.close()
