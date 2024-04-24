6ae69269f1b5990ea3ebd2727a8833066fb59b52
Андрей <amore1302@mail.ru>
2024-04-24 15:25:43 +0300

    create folder bd

---
+++ bd/restorans.py
@@ -0,0 +1,38 @@
+"""
+restaurants  таблица ресторанов
+Поля
+restaurant_id  id ресторана
+name название
+address адрес
+
+
+CREATE TABLE restaurants (
+    restaurant_id INTEGER PRIMARY KEY  AUTOINCREMENT,
+    name TEXT   NOT NULL,
+    address TEXT  NOT NULL
+);
+
+"""
+
+import sqlite3
+
+# Подключаемся к базе данных (или создаем новую)
+conn = sqlite3.connect('../samo_mag_bot.db')
+cursor = conn.cursor()
+
+# Создаем таблицу, если она еще не существует
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS restaurants (
+    restaurant_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT  NOT NULL,
+    address TEXT  NOT NULL
+);
+''')
+
+
+
+# Сохраняем изменения в базе данных и закрываем соединение
+conn.commit()
+conn.close()
+
+
---
+++ bd/review_rest.py
@@ -0,0 +1,53 @@
+"""
+review_rest таблица отзывов о ресторане
+Поля
+review_rest_id  id отзыва
+user_id  id пользователя кто сделал отзыв
+restaurant_id  id ресторана о котором  натисан отзыв
+dt дата в формате гггг-мм-дд:чч:мм
+descr сам отзыв
+
+
+
+CREATE TABLE review_rest (
+    review_rest_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    restaurant_id INTEGER NOT NULL,
+    dt TEXT  NOT NULL,
+    name TEXT  NOT NULL,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+)
+
+CREATE  INDEX idx_review_rest_dt ON review_rest (dt)
+
+"""
+
+
+import sqlite3
+
+# Подключаемся к базе данных (или создаем новую)
+conn = sqlite3.connect('../samo_mag_bot.db')
+cursor = conn.cursor()
+
+# Создаем  таблицу, если она еще не существует
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS review_rest (
+    review_rest_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    restaurant_id INTEGER NOT NULL,
+    dt TEXT  NOT NULL,
+    name TEXT  NOT NULL,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+)
+''')
+
+# Создаем уникальный индекс на поле dt
+cursor.execute('''
+CREATE  INDEX IF NOT EXISTS  idx_review_rest_dt ON review_rest (dt)
+''')
+
+# Сохраняем изменения в базе данных и закрываем соединение
+conn.commit()
+conn.close()
